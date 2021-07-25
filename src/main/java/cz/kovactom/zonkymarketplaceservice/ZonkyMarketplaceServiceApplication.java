b2603320d9418ebe112dc4629d0cee77144d61a6
kovactom <kovactom>
2021-07-25 20:23:16 +0200

    Initial commit

---
+++ src/main/java/cz/kovactom/zonkymarketplaceservice/ZonkyMarketplaceServiceApplication.java
@@ -0,0 +1,13 @@
+package cz.kovactom.zonkymarketplaceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZonkyMarketplaceServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZonkyMarketplaceServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/kovactom/zonkymarketplaceservice/ZonkyMarketplaceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.kovactom.zonkymarketplaceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZonkyMarketplaceServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
